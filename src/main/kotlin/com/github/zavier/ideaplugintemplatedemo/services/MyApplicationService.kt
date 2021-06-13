ce6288dc75685ddc418c12d875127408bddf0f1e
GitHub Action <action@github.com>
2021-06-13 02:29:02 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/zavier/ideaplugintemplatedemo/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.zavier.ideaplugintemplatedemo.services
+
+import com.github.zavier.ideaplugintemplatedemo.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
